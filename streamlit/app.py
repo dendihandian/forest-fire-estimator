e5a1f56f83bb592c50d429eb5d8cb811e1ee92a2
Dendi Handian <contact.dendi@yahoo.com>
2022-04-30 22:26:48 +0700

    test streamlit app

---
+++ streamlit/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Forest Fire Estimator', anchor=None)
+st.write("""
+    - [Exploratory Data Analysis](https://github.com/dendihandian/forest-fire-estimator/blob/main/forest-fire-estimator-premodeling.ipynb)
+    - [Dataset](https://archive.ics.uci.edu/ml/datasets/forest+fires)
+""")
